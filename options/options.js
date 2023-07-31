bf7fcc4c833e103e8426a97ea81e83851b7e246a
Crowdbotics <engineering@crowdbotics.com>
2023-07-31 18:23:15 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "raspy-waterfall-de-105860",
+  url: "https://raspy-waterfall-de-105860.botics.co",
+  api: "https://raspy-waterfall-de-105860.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
